6821a55bcae78bc3887007a7fbd76bb79688d02b
swordarrow2 <2856986197@qq.com>
2024-07-13 00:08:55 +0800

    range finder add "mini4"

--- inav7.1.2/src/main/io/rangefinder_benewake.c
+++ inav7.1.2/src/main/io/rangefinder_benewake.c
@@ -40,34 +40,56 @@
 #include "drivers/rangefinder/rangefinder_virtual.h"
 #include "drivers/time.h"
 #include "io/rangefinder.h"
-
+//AE A7 17 00 85 00 00 00 34 00 00 00 34 00 00 00 00 00 00 00 00 00 00 01 05 BC BE 
+// 0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f 10 11 12 13 14 15 16 17 18 19 1a
+// 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
 typedef struct __attribute__((packed)) {
-    uint8_t     header0;
-    uint8_t     header1;
-    uint8_t     distL;
-    uint8_t     distH;
-    uint8_t     strengthL;
-    uint8_t     strengthH;
-    uint8_t     res;
-    uint8_t     rawQual;
-    uint8_t     checksum;
-} tfminiPacket_t;
+    uint8_t     header0;//0xAE
+    uint8_t     header1;//0xA7
 
-#define BENEWAKE_PACKET_SIZE    sizeof(tfminiPacket_t)
-#define BENEWAKE_MIN_QUALITY    0
-#define BENEWAKE_TIMEOUT_MS     200 // 200ms
+    uint8_t     length;//23 0x17
+    uint8_t     addr; //0x00
+    uint8_t     cmd; //0x85
+    uint8_t     unuse1;
+    uint8_t     unuse2;
+    uint8_t     length1;
+    uint8_t     length2;
+    uint8_t     unuse5;
+    uint8_t     unuse6;
+    uint8_t     length1_1;
+    uint8_t     length1_2;
+    uint8_t     unuse9;
+    uint8_t     unuse10;
+    uint8_t     unuse11;
+    uint8_t     unuse12;
+    uint8_t     unuse13;
+    uint8_t     unuse14;
+    uint8_t     unuse15;
+    uint8_t     unuse16;
+    uint8_t     unuse17;
+    uint8_t     unuse18;
+    uint8_t     unit;
+    
+    uint8_t     checksum;
+    uint8_t     end1;
+    uint8_t     end2;
+} mini4Packet_t;
+
+#define MINI4_PACKET_SIZE    sizeof(mini4Packet_t)
+#define MINI4_MIN_QUALITY    0
+#define MINI4_TIMEOUT_MS     500 // 500ms
 
 static serialPort_t * serialPort = NULL;
 static serialPortConfig_t * portConfig;
-static uint8_t  buffer[BENEWAKE_PACKET_SIZE];
+static uint8_t  buffer[MINI4_PACKET_SIZE];
 static unsigned bufferPtr;
 static timeMs_t lastProtocolActivityMs;
 
 static bool hasNewData = false;
 static int32_t sensorData = RANGEFINDER_NO_NEW_DATA;
 
-// TFmini command to initiate 100Hz sampling
-static const uint8_t initCmd100Hz[] = { 0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x01, 0x06 };
+// TFmini command to initiate 2Hz sampling
+static const uint8_t initCmd2Hz[] = {0xAE , 0xA7 , 0x04 , 0x00 , 0x05 , 0x09 , 0xBC , 0xBE};
 
 static bool benewakeRangefinderDetect(void)
 {
@@ -85,7 +107,7 @@ static void benewakeRangefinderInit(void)
         return;
     }
 
-    serialPort = openSerialPort(portConfig->identifier, FUNCTION_RANGEFINDER, NULL, NULL, 115200, MODE_RXTX, SERIAL_NOT_INVERTED);
+    serialPort = openSerialPort(portConfig->identifier, FUNCTION_RANGEFINDER, NULL, NULL, 9600, MODE_RXTX, SERIAL_NOT_INVERTED);
     if (!serialPort) {
         return;
     }
@@ -96,67 +118,88 @@ static void benewakeRangefinderInit(void)
 
 static void benewakeRangefinderUpdate(void)
 {
+    uint8_t checksum = 0;
+    uint8_t flag = 0;
     // Initialize the sensor
-    if (lastProtocolActivityMs == 0 || (millis() - lastProtocolActivityMs) > BENEWAKE_TIMEOUT_MS) {
-        // Initialize the sensor to do 100Hz sampling to make sure we get the most recent data always
-        serialWriteBuf(serialPort, initCmd100Hz, sizeof(initCmd100Hz));
-
-        // Send the init command every BENEWAKE_TIMEOUT_MS
+    if (lastProtocolActivityMs == 0 || (millis() - lastProtocolActivityMs) > MINI4_TIMEOUT_MS) {
+        serialWriteBuf(serialPort, initCmd2Hz, sizeof(initCmd2Hz));
         lastProtocolActivityMs = millis();
-    }
 
     // Process incoming bytes
-    tfminiPacket_t *tfminiPacket = (tfminiPacket_t *)buffer;
+    mini4Packet_t *mini4Packet = (mini4Packet_t *)buffer;
     while (serialRxBytesWaiting(serialPort) > 0) {
         uint8_t c = serialRead(serialPort);
 
         // Add byte to buffer
-        if (bufferPtr < BENEWAKE_PACKET_SIZE) {
+        if (bufferPtr < MINI4_PACKET_SIZE) {
             buffer[bufferPtr++] = c;
         }
 
         // Check header bytes
-        if ((bufferPtr == 1) && (tfminiPacket->header0 != 0x59)) {
+        if ((bufferPtr == 1) && (mini4Packet->header0 != 0xAE)) {
             bufferPtr = 0;
             continue;
         }
 
-        if ((bufferPtr == 2) && (tfminiPacket->header1 != 0x59)) {
+        if ((bufferPtr == 2) && (mini4Packet->header1 != 0xA7)) {
+            bufferPtr = 0;
+            continue;
+        }
+
+        if ((bufferPtr == 3) && (mini4Packet->length == 0x4)) {
+            hasNewData = true;
+            lastProtocolActivityMs = millis();
+            sensorData = 1;
+            bufferPtr = 0;
+            continue;
+        }
+
+        if ((bufferPtr == 3) && (mini4Packet->length != 0x17)) {
+            bufferPtr = 0;
+            continue;
+        }
+
+        if ((bufferPtr == 26) && (mini4Packet->end1 != 0xBC)) {
+            bufferPtr = 0;
+            continue;
+        }
+
+        if ((bufferPtr == 27) && (mini4Packet->end2 != 0xBE)) {
             bufferPtr = 0;
             continue;
         }
 
         // Check for complete packet
-        if (bufferPtr == BENEWAKE_PACKET_SIZE) {
-            const uint8_t checksum = buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4] + buffer[5] + buffer[6] + buffer[7];
-            if (tfminiPacket->checksum == checksum) {
+        if (bufferPtr == MINI4_PACKET_SIZE) {
+            for(flag = 2;flag < MINI4_PACKET_SIZE - 3;flag++){
+                checksum += buffer[flag];
+            }
+            
+            if (mini4Packet->checksum == checksum) {
                 // Valid packet
                 hasNewData = true;
-                sensorData = (tfminiPacket->distL << 0) | (tfminiPacket->distH << 8);
+                sensorData = (mini4Packet->length1 << 8) | (mini4Packet->length2 << 0);
+                sensorData *= 10;
                 lastProtocolActivityMs = millis();
-
-                uint16_t qual = (tfminiPacket->strengthL << 0) | (tfminiPacket->strengthH << 8);
-
-                if (sensorData == 0 || qual <= BENEWAKE_MIN_QUALITY) {
-                    sensorData = -1;
-                }
             }
 
             // Prepare for new packet
             bufferPtr = 0;
         }
     }
+      }
 }
 
 static int32_t benewakeRangefinderGetDistance(void)
 {
-    if (hasNewData) {
-        hasNewData = false;
-        return (sensorData > 0) ? (sensorData) : RANGEFINDER_OUT_OF_RANGE;
-    }
-    else {
-        return RANGEFINDER_NO_NEW_DATA;
-    }
+    return (sensorData > 0) ? (sensorData) : 1;
+    // if (hasNewData) {
+    //     hasNewData = false;
+    //     return (sensorData > 0) ? (sensorData) : RANGEFINDER_OUT_OF_RANGE;
+    // }
+    // else {
+    //     return RANGEFINDER_OUT_OF_RANGE;
+    // }
 }
 
 virtualRangefinderVTable_t rangefinderBenewakeVtable = {
